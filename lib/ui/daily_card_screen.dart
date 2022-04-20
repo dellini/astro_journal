0b4087d8e7c0327295fa0cd2ccc50fe8a4697fba
dellini <dellini@bk.ru>
2022-04-20 19:17:54 +0300

    create daily card screen

--- lib/ui/daily_card_screen.dart
+++ lib/ui/daily_card_screen.dart
@@ -1,5 +1,6 @@
 import 'package:astro_journal/data/tarot_card.dart';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 class DailyCardScreen extends StatelessWidget {
   final TarotCard tarotCard;
@@ -10,14 +11,62 @@ class DailyCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Карта дня'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/background.png'),
+          fit: BoxFit.fitWidth,
+        ),
       ),
-      body: ListView(
-        children: [
-          Text(tarotCard.toString()),
-        ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            // ListView(
+            //   children: [
+            //     Container(
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(16),
+            //       ),
+            //     ),
+            //     Text(
+            //       tarotCard.toString(),
+            //       style: const TextStyle(color: Colors.white),
+            //     ),
+            //   ],
+            // ),
+            Positioned(
+              bottom: 0,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(512),
+                  topRight: Radius.circular(512),
+                ),
+                child: GlassmorphicContainer(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.551,
+                  borderRadius: 0,
+                  linearGradient: LinearGradient(
+                    colors: [
+                      const Color(0xffFFDE59).withOpacity(0.05),
+                      const Color(0xffFFDE59).withOpacity(0.4),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  border: 0,
+                  blur: 8,
+                  borderGradient: const LinearGradient(
+                    colors: [
+                      Color(0xffFFDE59),
+                      Colors.white,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
