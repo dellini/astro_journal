7f07fe545ed8fcf3dbae1e0846cb9c04ac2fb324
Darya <dellini@bk.ru>
2022-05-25 22:07:08 +0300

    add getting tarot card history

--- lib/repositories/hive_tarot_history_repository.dart
+++ lib/repositories/hive_tarot_history_repository.dart
@@ -1,9 +1,27 @@
 import 'package:astro_journal/data/tarot_card.dart';
 import 'package:astro_journal/data/tarot_card_dto.dart';
+import 'package:astro_journal/date_extensions.dart';
 import 'package:hive/hive.dart';
 
 class HiveTarotHistoryRepository {
   final Box<TarotCardDTO> box;
 
+  List<TarotCardDTO> get tarotCardHistory {
+    final finishDate = DateTime.now();
+    final startDate = finishDate.subtract(const Duration(days: 7)).date;
+    final periodHistory = box.values
+        .where((e) =>
+            e.createdAt.isBefore(finishDate) && e.createdAt.isAfter(startDate))
+        .toList()
+      ..sort(
+        (a, b) => a.createdAt.compareTo(b.createdAt),
+      );
+    return periodHistory;
+  }
+
   HiveTarotHistoryRepository(this.box);
+
+  Future<void> addCartToHistory(TarotCard card) async {
+    await box.add(card.dtoWithCreatedTime());
+  }
 }
