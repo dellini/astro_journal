8c945a57882a436772afbebed20dbf296c60e293
Darya <dellini@bk.ru>
2022-05-31 21:34:18 +0300

    add glassmophic bottom sheet

--- lib/ui/calendar_screen.dart
+++ lib/ui/calendar_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
@@ -25,51 +27,77 @@ class _CalendarScreenState extends State<CalendarScreen> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 32, 32, 32),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Builder(
-            builder: (context) {
-              return CalendarCarousel(
-                onDayPressed: (day, _) {
-                  showBottomSheet<void>(
-                    context: context,
-                    builder: (context) {
-                      return Scaffold(
-                        body: Container(
-                          height: 56,
-                          width: MediaQuery.of(context).size.width,
-                          color: Colors.amberAccent,
-                        ),
-                      );
-                    },
-                  );
-                },
-                headerMargin: const EdgeInsets.only(
-                  left: 84,
-                  right: 84,
-                  top: 28,
-                  bottom: 28,
-                ),
-                headerTextStyle: _textStyle.copyWith(
-                  color: Colors.amberAccent,
-                  fontSize: 22,
-                ),
-                daysTextStyle: _textStyle,
-                weekdayTextStyle: _textStyle,
-                weekendTextStyle:
-                    _textStyle.copyWith(color: Colors.amberAccent),
-                todayButtonColor: Colors.amberAccent,
-                todayTextStyle: _textStyle.copyWith(
-                  color: const Color.fromARGB(255, 32, 32, 32),
-                  fontWeight: FontWeight.bold,
-                ),
-                prevDaysTextStyle:
-                    _textStyle.copyWith(color: Colors.white38, fontSize: 14),
-                nextDaysTextStyle:
-                    _textStyle.copyWith(color: Colors.white38, fontSize: 14),
-                iconColor: Colors.amberAccent,
-              );
-            },
+        child: SlidingUpPanel(
+          maxHeight: MediaQuery.of(context).size.height * 0.9,
+          minHeight: MediaQuery.of(context).size.height * 0.3,
+          renderPanelSheet: false,
+          panel: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(512),
+              topRight: Radius.circular(512),
+            ),
+            child: GlassmorphicContainer(
+              width: MediaQuery.of(context).size.width,
+              height: double.infinity,
+              borderRadius: 0,
+              linearGradient: LinearGradient(
+                colors: [
+                  const Color(0xffFFDE59).withOpacity(0.05),
+                  const Color(0xffFFDE59).withOpacity(0.4),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+              border: 0,
+              blur: 8,
+              borderGradient: const LinearGradient(
+                colors: [
+                  Color(0xffFFDE59),
+                  Colors.white,
+                ],
+              ),
+            ),
+          ),
+          // header: Container(
+          //   height: 60,
+          //   width: 360,
+          //   color: Colors.red,
+          // ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Builder(
+              builder: (context) {
+                return CalendarCarousel(
+                  locale: 'ru_RU',
+                  pageScrollPhysics: const NeverScrollableScrollPhysics(),
+                  onDayPressed: (day, _) {},
+                  headerMargin: const EdgeInsets.only(
+                    left: 84,
+                    right: 84,
+                    top: 28,
+                    bottom: 28,
+                  ),
+                  headerTextStyle: _textStyle.copyWith(
+                    color: Colors.amberAccent,
+                    fontSize: 22,
+                  ),
+                  daysTextStyle: _textStyle,
+                  weekdayTextStyle: _textStyle,
+                  weekendTextStyle:
+                      _textStyle.copyWith(color: Colors.amberAccent),
+                  todayButtonColor: Colors.amberAccent,
+                  todayTextStyle: _textStyle.copyWith(
+                    color: const Color.fromARGB(255, 32, 32, 32),
+                    fontWeight: FontWeight.bold,
+                  ),
+                  prevDaysTextStyle:
+                      _textStyle.copyWith(color: Colors.white38, fontSize: 14),
+                  nextDaysTextStyle:
+                      _textStyle.copyWith(color: Colors.white38, fontSize: 14),
+                  iconColor: Colors.amberAccent,
+                );
+              },
+            ),
           ),
         ),
       ),
